1438f7271ff35a2aa7ac006b33f6d9d17e306016
JeremyDenver78 <jeremy@pocketmotionpictures.com>
2025-12-11 19:34:15 -0700

    Initial Commit

---
+++ VibeFocusApp/VibeFocusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeFocusAppApp.swift
+//  VibeFocusApp
+//
+//  Created by Jeremy E on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct VibeFocusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VibeFocusAppTests/VibeFocusAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  VibeFocusAppTests.swift
+//  VibeFocusAppTests
+//
+//  Created by Jeremy E on 12/11/25.
+//
+
+import Testing
+@testable import VibeFocusApp
+
+struct VibeFocusAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
